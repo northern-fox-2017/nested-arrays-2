1d0ccda65144a000fc7ef4234f23989a8faaaa3e
idabagusangga <angga.idabagus@gmail.com>
2017-11-15 16:39:23 +0700

    recursive?

--- nested_array_2_convert.js
+++ nested_array_2_convert.js
@@ -11,16 +11,42 @@ let roster = [
 
 // [[roster[0][0], roster[1][0]], [roster[0][1], roster[1][1]],...
 
-function convert_roster_format (nestedArray) {
-  // your convert code here
+function convert_roster_format (nestedArray,arr = []) {
+  // let arr = []
+  // for(let i = 1; i < roster.length;i++){
+  //   let rosterObj = {}
+  //   for(let j = 0; j < roster[i].length;j++){
+  //     rosterObj[roster[0][j]] = roster[i][j]
+  //   }
+  //   arr.push(rosterObj)
+  // }
+  // return arr
+  let rosterObj = {}
+  let i = 0
+  while(i < roster[0].length){
+    rosterObj[roster[0][i]] = roster[1][i]
+    i++
+  }
+  console.log(nestedArray)
+  arr.push(rosterObj)
+  console.log(arr)
+  var a = nestedArray.splice(1,1)
+  console.log(a)
+  console.log(nestedArray)
+  if(roster.length == 1){
+    return arr
+  }
+  else{
+    return convert_roster_format(nestedArray,arr)
+  }
 }
 
 let object_roster = convert_roster_format(roster)
-console.log(object_roster[2])
+console.log(object_roster)
 
 // => { "Number": 31, "Name": "Harvey Kay", "Position": "Shooting Guard", "Points per Game": [0, 30, 16, 0, 25] }
 
-console.log(object_roster[0]['Name'] == 'Joe Schmo') // outputs true
+// console.log(object_roster[0]['Name'] == 'Joe Schmo') // outputs true
 
 module.exports = {
   convert_roster_format
