2a5ebf4ed3be3e02d84d236e5a0c7f415fa9ab6c
harisman <harismannugraha@gmail.com>
2017-11-15 21:00:00 +0700

    di perbarui kembali

--- nested_array_2_convert.js
+++ nested_array_2_convert.js
@@ -15,22 +15,19 @@ function convert_roster_format (nestedArray) {
   let result = [];
   let obj = {};
   for (var i = 1; i < nestedArray.length; i++) {
-
-    obj[nestedArray[0][0]] = nestedArray[i][0]
-    obj[nestedArray[0][1]] = nestedArray[i][1]
-    obj[nestedArray[0][2]] = nestedArray[i][2]
-    obj[nestedArray[0][3]] = nestedArray[i][3]
-    result.push(obj);
     obj = {};
-    // console.log(obj);
-  }
 
+    for (var j = 0; j < nestedArray[0].length; j++){
+    obj[nestedArray[0][j]] = nestedArray[i][j]
+
+  }
+  result.push(obj);
+}
   return result;
 }
 
 let object_roster = convert_roster_format(roster)
 console.log(object_roster[2])
-// console.log(object_roster)
 // => { "Number": 31, "Name": "Harvey Kay", "Position": "Shooting Guard", "Points per Game": [0, 30, 16, 0, 25] }
 
 console.log(object_roster[0]['Name'] == 'Joe Schmo') // outputs true
