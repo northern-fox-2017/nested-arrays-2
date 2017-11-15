5b04b7baf721d3a232dc87fc04f80100e89052ce
Andrey Amrulloh <andrey.amrulloh@gmail.com>
2017-11-15 16:13:11 +0700

    Origin 1 Andrey Amrulloh Nested array 2

--- nested_array_2_convert.js
+++ nested_array_2_convert.js
@@ -8,14 +8,26 @@ let roster = [
   [7, 'Sally Talls', 'Power Forward ', [18, 29, 26, 31, 19] ],
   [22, 'MK DiBoux ', 'Small Forward ', [11, 0, 23, 17, 0] ]
 ]
-
+console.log(roster[0][0])
 // [[roster[0][0], roster[1][0]], [roster[0][1], roster[1][1]],...
 
 function convert_roster_format (nestedArray) {
-  // your convert code here
+	let result = [];
+	let obj = {};
+
+	for(let i = 1; i < nestedArray.length; i++){
+		for(let j = 0; j < nestedArray[i].length; j++){
+			obj[nestedArray[0][j]] = nestedArray[i][j];
+		}
+		result.push(obj);
+		obj = {};
+	}
+	return result;
 }
 
+
 let object_roster = convert_roster_format(roster)
+
 console.log(object_roster[2])
 
 // => { "Number": 31, "Name": "Harvey Kay", "Position": "Shooting Guard", "Points per Game": [0, 30, 16, 0, 25] }
